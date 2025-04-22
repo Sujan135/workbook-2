8d30c74ed194aaba2e311036464dd0ab2d7ae1a9
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-22 10:18:03 -0500

    Add median calculation and show difference from average

--- TestStatistics/src/main/java/com/pluralsight/TestStatistics.java
+++ TestStatistics/src/main/java/com/pluralsight/TestStatistics.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
 public class TestStatistics {
     public static void main(String[] args) {
         int [] scores = {85, 92, 76, 88, 95, 79, 82, 94, 73, 86};
@@ -18,10 +20,21 @@ public class TestStatistics {
             if (score < low) low = score;
         }
 
+        Arrays.sort(scores);
+        double median;
+        int mid = scores.length/2;
+        if (scores.length % 2 ==0) {
+            median = (scores[mid - 1] + scores[mid]) / 2.0;
+        }else {
+            median = scores[mid];
+        }
+
         System.out.println("Test Scores Statistics:");
         System.out.printf("Average: %.2f%n", average);
         System.out.println("High Score: " + high);
         System.out.println("Low Score: " + low);
+        System.out.printf("Median: %.2f%n", median);
+        System.out.printf("Difference between average and median: %.2f%n", Math.abs(average - median));
 
     }
 }
